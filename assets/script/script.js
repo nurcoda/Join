06ba4b0bb4089f32b401ff3f238cb1dade00cd47
Joel <sygullajoel@gmail.com>
2024-06-07 21:12:01 +0200

    header improved

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,19 +1,18 @@
 async function includeHTML() {
-   let includeElements = document.querySelectorAll("[w3-include-html]");
-   for (let i = 0; i < includeElements.length; i++) {
-      const element = includeElements[i];
-      file = element.getAttribute("w3-include-html"); // "includes/header.html"
-      let resp = await fetch(file);
-      if (resp.ok) {
-         element.innerHTML = await resp.text();
-      } else {
-         element.innerHTML = "Page not found";
-      }
-   }
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html"); // "includes/header.html"
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
+    }
+  }
 }
 
 function showHeaderNav() {
-   let hiddenElement = document.getElementById('headerNav');
-   hiddenElement.style.display = 'flex';
+  let hiddenElement = document.getElementById("headerNav");
+  hiddenElement.classList.toggle("d-none");
 }
-
