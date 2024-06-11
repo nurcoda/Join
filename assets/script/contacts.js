9abd9019bc5d3f8dded1d118ef7d8ea297784909
nurcoda <mailo.mittelaedt@googlemail.com>
2024-06-11 17:36:42 +0200

    add Div Struktur Contact Card Big

--- assets/script/contacts.js
+++ assets/script/contacts.js
@@ -1,63 +1,63 @@
-let Contacts = [
+let contacts = [
     {
         "phonenumber": 876543221,
-        "Email": "ander@anders.de",
-        "Name": "Anders Anderson",
+        "email": "ander@anders.de",
+        "name": "Anders Anderson",
     },
     {
         "phonenumber": 555696969,
-        "Email": "bunny@lola.de",
-        "Name": "Lola Bunny",
+        "email": "bunny@lola.de",
+        "name": "Lola Bunny",
     },
     {
         "phonenumber": 876543221,
-        "Email": "ander@anders.de",
-        "Name": "Anders Anderson",
+        "email": "ander@anders.de",
+        "name": "Anders Anderson",
     },
     {
         "phonenumber": 555696969,
-        "Email": "bunny@lola.de",
-        "Name": "Lola Bunny",
+        "email": "bunny@lola.de",
+        "name": "Lola Bunny",
     },
     {
         "phonenumber": 876543221,
-        "Email": "ander@anders.de",
-        "Name": "Anders Anderson",
+        "email": "ander@anders.de",
+        "name": "Anders Anderson",
     },
     {
         "phonenumber": 555696969,
-        "Email": "bunny@lola.de",
-        "Name": "Lola Bunny",
+        "email": "bunny@lola.de",
+        "name": "Lola Bunny",
     },
     {
         "phonenumber": 876543221,
-        "Email": "ander@anders.de",
-        "Name": "Anders Anderson",
+        "email": "ander@anders.de",
+        "name": "Anders Anderson",
     },
     {
         "phonenumber": 555696969,
-        "Email": "bunny@lola.de",
-        "Name": "Lola Bunny",
+        "email": "bunny@lola.de",
+        "name": "Lola Bunny",
     },
     {
         "phonenumber": 876543221,
-        "Email": "ander@anders.de",
-        "Name": "Anders Anderson",
+        "email": "ander@anders.de",
+        "name": "Anders Anderson",
     },
     {
         "phonenumber": 555696969,
-        "Email": "bunny@lola.de",
-        "Name": "Lola Bunny",
+        "email": "bunny@lola.de",
+        "name": "Lola Bunny",
     },
     {
         "phonenumber": 876543221,
-        "Email": "ander@anders.de",
-        "Name": "Anders Anderson",
+        "email": "ander@anders.de",
+        "name": "Anders Anderson",
     },
     {
         "phonenumber": 555696969,
-        "Email": "bunny@lola.de",
-        "Name": "Lola Bunny",
+        "email": "bunny@lola.de",
+        "name": "Lola Bunny",
     },
 
 ];
@@ -70,18 +70,18 @@ function renderContact() {
               <div id="avatar${i}" class="avatar"></div>
            </div>
            <div class="contacts-content-list">
-              <span>${Contacts[i]["Name"]}</span>
-              <a href="">Email: ${Contacts[i]["Email"]}</a>
+              <span>${contacts[i]["name"]}</span>
+              <a href="">Email: ${contacts[i]["email"]}</a>
            </div>
 </div>
     `;
-    renderAvatar(i);
+        renderAvatar(i);
     }
 }
 
-function renderAvatar(i){
+function renderAvatar(i) {
     const avatar = document.getElementById(`avatar${i}`)
-    const username = Contacts[i]["Name"] // will be fetching the username, just used my to illustrate
+    const username = contacts[i]["name"] // will be fetching the username, just used my to illustrate
     const firstNameInitial = username[0]
     const secondNameInitial = username.split(' ')[1].split('')[0]
     avatar.innerHTML += firstNameInitial + secondNameInitial
