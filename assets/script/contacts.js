344bb91b72c8ece3e484efffe2f601518acd4a58
Jonas Mahlburg <darrylmanga@yahoo.com>
2024-06-11 11:55:50 +0200

    added JavaScript  Contact Part1

---
+++ assets/script/contacts.js
@@ -0,0 +1,26 @@
+let Contacts= [
+    {
+    "phonenumber": 876543221,
+    "Email": "ander@anders.de",
+    "Name": "Anders Anderson",
+    },
+    {
+        "phonenumber": 555696969,
+        "Email": "bunny@lola.de",
+        "Name": "Lola Bunny",
+        },
+
+];
+
+function renderContact(){
+    for(let i=0; i<Contacts.length; i++){
+           document.getElementById("contactList").innerHTML= `<div class="contact">
+   <div>
+      name${Email[i]}
+      email${i}
+   </div>
+</div>
+    `
+    }
+ 
+}
