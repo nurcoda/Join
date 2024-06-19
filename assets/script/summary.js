5dc3ee98814de5c219c450f30c27d3e6e50b12bd
nurcoda <mailo.mittelaedt@googlemail.com>
2024-06-19 15:39:28 +0200

    first function

--- assets/script/summary.js
+++ assets/script/summary.js
@@ -1,1 +1,7 @@
 let Test = "./assets/script/testData.js";
+
+function tasksInBoardCounter(tasks) {
+    let taskCount = tasks.length;
+    console.log("Anzahl der erstellten Tasks: " + taskCount);
+    return taskCount;
+}
