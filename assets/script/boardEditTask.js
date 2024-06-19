e42bba6d7603692951aa41a67def9440b62fd3e4
br4hm <haehnlein.alexander@gmx.de>
2024-06-19 15:51:06 +0200

    edit render ausgelagert

--- assets/script/boardEditTask.js
+++ assets/script/boardEditTask.js
@@ -1,17 +1,144 @@
-function showAssignedDropdown() {
-   document.getElementById("assignedDropdown").classList.remove("d-none");
-   showOpenedDropdownIcon();
+// +++ !!! +++ !!! +++
+// DONT TOUCH THIS
+// +++ !!! +++ !!! +++
+
+function initRenderAssignedContacts(i) {
+   let assignedUserHTML = "";
+   assignedContacts = [];
+   tasks[i].assigned_user.forEach((user) => {
+      let user_id = getUserIdByName(user.name);
+      let assignedUser = getUserObjectById(user_id);
+      assignedContacts.push(assignedUser);
+      let assignedUserColor = getColorAssignedUser(user.name);
+      assignedUserHTML += `
+       <div class="assigned-contacts-img" style="background-color: ${assignedUserColor}; color:white;" data-id="${user_id}">
+          ${user.first_two_letters}
+       </div>
+       `;
+   });
+   return assignedUserHTML;
 }
 
 function renderAssignedContactsPopUp(i) {
+   assignedContacts = [];
    let assignedUser = "";
    tasks[i].assigned_user.forEach((user) => {
+      let user_id = getUserIdByName(user.name);
+      let assignedUser = getUserObjectById(user_id);
+      assignedContacts.push(assignedUser);
       let assignedUserColor = getColorAssignedUser(user.name);
       assignedUser += `
-       <div class="assigned-contacts-img" style="background-color: ${assignedUserColor}; color:white;">${user.first_two_letters}</div>
+       <div class="assigned-contacts-img" style="background-color: ${assignedUserColor}; color:white;" data-id="${user_id}">
+          ${user.first_two_letters}
+       </div>
        `;
    });
    return assignedUser;
+}
+
+function getUserObjectById(user_id) {
+   return contacts.find((user) => user.id === user_id);
+}
+/**
+ * render dropdown contactlist to assign user
+ * checks, assigned user and marks them
+ * @param {*} i
+ */
+
+function renderContactsDropdownPopUpEdit(i) {
+   let dropdown = document.getElementById("assignedDropdown");
+   dropdown.innerHTML = "";
+
+   for (let j = 0; j < contacts.length; j++) {
+      let isAssigned = false;
+      let imgSrc = "./assets/img/check_btn.png";
+      for (let k = 0; k < tasks[i].assigned_user.length; k++) {
+         let assigned_user_id = getUserIdByName(tasks[i].assigned_user[k].name);
+         if (assigned_user_id === contacts[j].id) {
+            isAssigned = true;
+            imgSrc = "./assets/img/checked_btn_white_svg.svg";
+            break;
+         }
+      }
+
+      dropdown.innerHTML += `
+         <div id="contact${contacts[j].id}" class="assigned-to-contact ${
+         isAssigned ? "marked" : ""
+      }" onclick="markContactEditPopUp(${contacts[j].id}, ${j})">
+            <div class="contact-img-name">
+               <div class="two-letters-img" style="background-color: ${contacts[j].color}; color: white;">
+                  ${contacts[j].first_two_letters}
+               </div>
+               <span>${contacts[j].name}</span>
+            </div>
+            <img id="contactCheckBtn${contacts[j].id}" class="check-btn-img" src="${imgSrc}" alt="" />
+         </div>`;
+   }
+}
+
+function getUserIdByName(userName) {
+   const user = contacts.find((user) => user.name.includes(userName));
+   return user ? user.id : null;
+}
+
+function markContactEditPopUp(contactId, i) {
+   let contact = document.getElementById(`contact${contactId}`);
+   let checkbox = document.getElementById(`contactCheckBtn${contactId}`);
+   contact.classList.toggle("marked");
+   renderAssignedContactsEditPopUp();
+
+   if (contact.classList.contains("marked")) {
+      contact.style.backgroundColor = "#2A3647";
+      contact.style.color = "white";
+      checkbox.src = "./assets/img/checked_btn_white_svg.svg";
+      addContactToAssignedEditPopUp(contactId);
+   } else {
+      contact.style.color = "black";
+      contact.style.backgroundColor = "white";
+      checkbox.src = "./assets/img/check_btn.png";
+      removeContactFromAssignedEditPopUp(contactId);
+   }
+}
+
+function addContactToAssignedEditPopUp(contactId) {
+   let contact = contacts.find((c) => c.id === contactId);
+   if (contact && !assignedContacts.some((c) => c.id === contactId)) {
+      assignedContacts.push(contact);
+      renderSingleAssignedContact(contact);
+   }
+}
+
+function removeContactFromAssignedEditPopUp(contactId) {
+   let index = assignedContacts.findIndex((c) => c.id === contactId);
+   if (index !== -1) {
+      assignedContacts.splice(index, 1);
+      document.querySelector(`#assignedContacts .assigned-contacts-img[data-id="${contactId}"]`).remove();
+   }
+}
+
+function renderSingleAssignedContact(contact) {
+   let content = document.getElementById("assignedContacts");
+   content.innerHTML += `
+      <div class="assigned-contacts-img" data-id="${contact.id}" style="background-color: ${contact.color}; color: white;">
+         ${contact.first_two_letters}
+      </div>`;
+}
+
+function renderAssignedContactsEditPopUp() {
+   let content = document.getElementById("assignedContacts");
+   content.innerHTML = "";
+   for (let i = 0; i < assignedContacts.length; i++) {
+      const contact = assignedContacts[i];
+      content.innerHTML += `
+         <div class="assigned-contacts-img" data-id="${contact.id}" style="background-color: ${contact.color}; color: white;">
+            ${contact.first_two_letters}
+         </div>`;
+   }
+}
+
+function showAssignedDropdown() {
+   document.getElementById("assignedDropdown").classList.remove("d-none");
+   showOpenedDropdownIcon();
 }
 
 function hideAssignedDropdown() {
@@ -28,91 +155,4 @@ function showOpenedDropdownIcon() {
 function hideOpenedDropdownIcon() {
    document.getElementById("dropdown2").classList.add("d-none");
    document.getElementById("dropdown1").classList.remove("d-none");
-}
-
-// document.getElementById("assignedDiv").addEventListener("click", function () {
-//    document.getElementById("assignedInput").focus(); // Setzt den Fokus auf das Input-Feld
-// });
-
-let assignedContacts = [];
-
-function renderContacts() {
-   let dropdown = document.getElementById("assignedDropdown");
-   let searchInput = document.getElementById("assignedInput").value.toLowerCase(); // Wert des Suchfelds
-
-   dropdown.innerHTML = ""; // Bereinigen des Dropdown-Inhalts
-
-   // Sortieren der Kontakte alphabetisch nach Namen
-   const sortedContacts = contacts.sort((a, b) => a.name.localeCompare(b.name));
-
-   for (let i = 0; i < contacts.length; i++) {
-      const contact = contacts[i];
-      const isAssigned = assignedContacts.includes(contact); // Prüfen, ob der Kontakt markiert ist
-      const contactClass = isAssigned ? "assigned-to-contact marked" : "assigned-to-contact"; // Klasse basierend auf Zuweisungsstatus
-      const imgSrc = isAssigned ? "./assets/img/checked_btn_white_svg.svg" : "./assets/img/check_btn.png";
-
-      // Überprüfen, ob der Kontaktname dem Suchkriterium entspricht
-      if (contact.name.toLowerCase().includes(searchInput)) {
-         // Erstellen des HTML-Strings für jeden Kontakt
-         dropdown.innerHTML += `
-          <div id="contact${contact.id}" class="${contactClass}" onclick="markContact(${contact.id}, ${i})">
-            <div class="contact-img-name">
-              <div class="two-letters-img" style="background-color: ${contact.color}; color: white;">
-                ${contact.first_two_letters}
-              </div>
-              <span>${contact.name}</span>
-            </div>
-            <img id="contactCheckBtn${contact.id}" class="check-btn-img" src="${imgSrc}" alt="" />
-          </div>`;
-      }
-   }
-}
-
-function markContact(contactId, i) {
-   let contact = document.getElementById(`contact${contactId}`);
-   let checkbox = document.getElementById(`contactCheckBtn${contactId}`);
-
-   // Umschalten der Markierungsklasse
-   contact.classList.toggle("marked");
-
-   // Prüfen, ob die Klasse vorhanden ist, und dementsprechend das Bild ändern
-   if (contact.classList.contains("marked")) {
-      contact.style.backgroundColor = "#2A3647";
-      contact.style.color = "white";
-      checkbox.src = "./assets/img/checked_btn_white_svg.svg";
-      addContactToAssigned(i);
-   } else {
-      contact.style.color = "black";
-      contact.style.backgroundColor = "white";
-      checkbox.src = "./assets/img/check_btn.png";
-      removeContactFromAssigned(i);
-   }
-}
-
-function addContactToAssigned(i) {
-   let contact = contacts[i];
-   assignedContacts.push(contact);
-   renderAssignedContacts();
-}
-
-function renderAssignedContacts() {
-   let content = document.getElementById("assignedContacts");
-   content.innerHTML = "";
-   for (let i = 0; i < assignedContacts.length; i++) {
-      const contact = assignedContacts[i];
-      content.innerHTML += `
-      <div class="assigned-contacts-img" style="background-color: ${contact.color}; color: white">
-      ${contact.first_two_letters}
-    </div>
-      `;
-   }
-}
-
-function removeContactFromAssigned(i) {
-   let contact = contacts[i];
-   let index = assignedContacts.findIndex((c) => c.id === contact.id);
-   if (index !== -1) {
-      assignedContacts.splice(index, 1);
-   }
-   renderAssignedContacts();
 }
