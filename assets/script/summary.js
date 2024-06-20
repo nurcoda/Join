df35e5b5be8e42ef669aa75f13f5342a0fd82a95
nurcoda <mailo.mittelaedt@googlemail.com>
2024-06-20 18:31:08 +0200

    task counter funktion

--- assets/script/summary.js
+++ assets/script/summary.js
@@ -1,6 +1,16 @@
-
-function tasksInBoardCounter(tasks) {
-    let taskCount = tasks.length;
-    console.log("Anzahl der erstellten Tasks: " + taskCount);
-    return taskCount;
+function tasksInBoardCounter() {
+    let taskNumbers = document.querySelectorAll('.taskNumber');// Alle Elemente mit der Klasse "taskNumber" auswählen
+    let totalTasks = 0;// Zähler für die Gesamtanzahl der Task
+    for (let i = 0; i < taskNumbers.length; i++) {
+        totalTasks += parseInt(taskNumbers[i].textContent, 10); // Schleife durch die ausgewählten Elemente und addiere deren Textinhalt zum Zähler
+    }
+    console.log(`Total tasks in board: ${totalTasks}`);// Ausgabe der Gesamtanzahl der Tasks (kann angepasst werden)
+    let totalTasksElement = document.getElementById('totalTasks');
+    if (totalTasksElement) {
+        totalTasksElement.innerHTML = `<p class="taskNumber" id="totalTasks">${totalTasks}</p>`
+    }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(tasksInBoardCounter, 1000); // 1 Sekunde Verzögerung, um sicherzustellen, dass alle Inhalte geladen sind
+});
